5c2dc748d35b4e797665444a142e522367c04121
Kevin Souris <kevin.souris@gmail.com>
2020-12-05 00:14:56 +0100

    update windows makefile

--- Makefile.bat
+++ Makefile.bat
@@ -8,9 +8,9 @@ echo OPTIONS = -Qstd=c99 -MT >> Makfile.mak
 echo MIC_ENABLED = -Qmic >> Makfile.mak
 
 echo INTEL64LIB = "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\compiler\lib\intel64" >> Makfile.mak
-echo VCLIB = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\lib\x64" >> Makfile.mak
-echo WINSDK1 = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\ucrt\x64" >> Makfile.mak
-echo WINSDK2 = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64" >> Makfile.mak
+echo VCLIB = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.16.27023\lib\x64" >> Makfile.mak
+echo WINSDK1 = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\ucrt\x64" >> Makfile.mak
+echo WINSDK2 = "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17763.0\um\x64" >> Makfile.mak
 echo LIB_PATH = -link -libpath:$(INTEL64LIB)  -link -libpath:$(VCLIB)  -link -libpath:$(WINSDK1)  -link -libpath:$(WINSDK2) >> Makfile.mak
 
 
