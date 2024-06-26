fa408db31e0cd39a4498e2dfa8d9290c56fb26ff
Kevin Souris <kevin.souris@iba-group.com>
2024-06-26 16:31:32 +0200

    fix warning when struct directory is not present

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ The MCsquare software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
 
 #include "include/define.h"
 #include "include/struct.h"
+#include "include/File_process.h"
 #include "include/data_config.h"
 #include "include/data_materials.h"
 #include "include/data_ct.h"
@@ -87,7 +88,8 @@ int main(int argc, char *argv[]){
   else Fields = NULL;
   
   // Import all contours
-  config.StructList = load_all_structs();
+  if((Directory_exists("./structs")) == 1) 
+    config.StructList = load_all_structs();
   
   // Import CT image(s)
   DATA_CT **CT_phases = NULL;
